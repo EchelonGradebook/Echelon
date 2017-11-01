5c198244fa5f04ec1e1632d5e7945b24555dce90
Justin Mennig <jjcm98@gmail.com>
2017-11-01 14:20:26 -0400

    Stored procedure class

---
+++ Echelon/Classes/StoredProcedue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Echelon.Classes
+{
+    public class StoredProcedue
+    {
+        protected string ConStr = "Server=tcp:echelongradebook.database.windows.net,1433;Initial Catalog=GradeBook;Persist Security Info=False;User ID=joshpadams123;Password=Jpa12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public void First(string user)
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+            try
+            {
+                conn = new SqlConnection(ConStr);
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("First", conn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                rdr = cmd.ExecuteReader();
+
+                //output here
+            }
+            finally{closeDB(conn, rdr);}
+        }
+
+        public void closeDB(SqlConnection conn, SqlDataReader rdr)
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+        }
+    }
+}
