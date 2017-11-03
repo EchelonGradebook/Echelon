10c46485686e0594c59a9357aa86de532278c9ce
joshpadams123 <31546513+joshpadams123@users.noreply.github.com>
2017-11-03 14:11:00 -0400

    Course Form, but doesn't Write to the database

---
+++ Echelon/Data/EfCourseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Echelon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Echelon.Data
+{
+    public class EfCourseRepository : ICourseRepository
+    {
+        private CourseDbContext context;
+        public EfCourseRepository(CourseDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<Course> Course => context.Course;
+    }
+}
---
+++ Echelon/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Echelon.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Id { get; set; }
+
+    }
+}
